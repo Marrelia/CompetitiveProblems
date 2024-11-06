6e59537ebc6becce59c3f531889e3481aec40d87
Marrelia <marrelia.orangecat@gmail.com>
2024-11-05 19:44:10 -0500

    Add changes to some codes

--- Atcoder/Beginner Contest/378/E.cpp
+++ Atcoder/Beginner Contest/378/E.cpp
@@ -17,8 +17,8 @@ void add(const int idx, const int k) {
 long long query(int idx) {
   long long res = 0;
   while (idx > 0) {
-	res += t[idx];
-	idx -= lowbit(idx);
+    res += t[idx];
+    idx -= lowbit(idx);
   }
   return res;
 }
@@ -26,22 +26,22 @@ long long query(int idx) {
 int main() {
   scanf("%d%d", &n, &m);
   for (int i = 1; i <= n; ++i) {
-	scanf("%d", &s[i]);
-	s[i] = (s[i - 1] + s[i]) % m;
+    scanf("%d", &s[i]);
+    s[i] = (s[i - 1] + s[i]) % m;
   }
 
   long long cnt = 0;
   for (int i = 1; i <= n; ++i) {
-	cnt += query(m) - query(s[i] + 1);
-	add(s[i] + 1, 1);
+    cnt += query(m) - query(s[i] + 1);
+    add(s[i] + 1, 1);
   }
 
   long long ans = 0;
   for (int i = 1; i <= n; ++i) {
-	ans += 1ll * i * s[i];
+    ans += 1ll * i * s[i];
   }
   for (int i = 1; i < n; ++i) {
-	ans -= 1ll * (n - i) * s[i];
+    ans -= 1ll * (n - i) * s[i];
   }
   ans += 1ll * cnt * m;
   printf("%lld\n", ans);
--- Other/ChemicalEquationBalancer.cpp
+++ Other/ChemicalEquationBalancer.cpp
@@ -2,19 +2,24 @@
  * A tool to balance the chemical Equation
  * You would input the chemical equation with their chemical formula, separate by +, and reactants and products are seprated by ->
  * Example:
- * CO2 + H2O -> O2 + C6H12O6
+ * 6 CO2 + 6 H2O -> 6 O2 + 1 C6H12O6
  *
  * Output will be the coefficients for each chemical substances
  * For above example:
  * 6 6 6 1
  *
- * No brackets support yet, may add later on :D
  */
 
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <map>
 #include <vector>
+#include <cstring>
+#include <stack>
+
+const double eps = 1e-6;
 
 int a[2][150][300];
 int len[2];
@@ -25,133 +30,194 @@ int cheNameToNum_cnt;
 std::map<std::string, int> cheNameToNum;
 std::string numToCheName[300];
 
-int addStringToMap(const std::string&);
+int gcd(int, int);
+
+// add a element name to map
+int addStringToMap(const std::string &);
+
+// process a chemical formula
+void process(int, int, std::string);
+
+// read the input
 void read();
+
+// convert the input to matrix
 void convertToMatrix();
+
+// solve the equation set
 std::vector<int> solve(int, int);
 
 int main() {
   read();
   convertToMatrix();
-  std::vector<int> ans = solve(cheNameToNum_cnt, len[0] + len[1]);
-  if (ans[0] == -1) {
-	puts("No Solution");
+  if (const std::vector<int> ans = solve(cheNameToNum_cnt, len[0] + len[1]); ans[0] == -1) {
+    puts("No Solution");
   } else {
-	for (int i = 1; i <= len[0] + len[1]; ++i)
-	  printf("%d ", ans[i]);
+    for (int i = 1; i <= len[0] + len[1]; ++i)
+      printf("%d ", ans[i]);
   }
   return 0;
 }
 
 int gcd(int x, int y) {
   while (y) {
-	std::swap(x, y);
-	y %= x;
+    std::swap(x, y);
+    y %= x;
   }
   return x;
 }
 
-int addStringToMap(const std::string& x) {
+int addStringToMap(const std::string &x) {
   if (cheNameToNum.contains(x)) return cheNameToNum[x];
   cheNameToNum[x] = ++cheNameToNum_cnt;
   numToCheName[cheNameToNum_cnt] = x;
   return cheNameToNum_cnt;
 }
 
+void process(const int x, const int y, std::string str) {
+  std::stack<char> stk;
+  // num of each element
+  int dep = 1, cnt[300][300] = {};
+  stk.push('(');
+  str.push_back(')');
+  for (int i = 0; i < str.length(); ++i) {
+    if (str[i] == ')') {
+      std::string tmp;
+      while (stk.top() != '(') {
+        tmp += stk.top();
+        stk.pop();
+      }
+      stk.pop();
+      std::ranges::reverse(tmp);
+      for (int k = 0; k < tmp.length();) {
+        std::string cur;
+        int num = 0;
+        cur += tmp[k++];
+        while (islower(tmp[k])) cur += tmp[k++];
+        while (isdigit(tmp[k])) num = (num << 3) + (num << 1) + tmp[k++] - '0';
+        if (!num) ++num;
+        cnt[dep][addStringToMap(cur)] += num;
+      }
+      int num = 0;
+      ++i;
+      while (isdigit(str[i])) num = (num << 3) + (num << 1) + str[i++] - '0';
+      --i;
+      if (!num) ++num, ++i;
+      for (int k = 1; k <= cheNameToNum_cnt; ++k) {
+        cnt[dep - 1][k] += cnt[dep][k] * num;
+      }
+      --dep;
+    } else {
+      stk.push(str[i]);
+    }
+    if (str[i] == '(') {
+      ++dep;
+      memset(cnt[dep], 0, sizeof cnt[dep]);
+    }
+  }
+
+  for (int i = 1; i <= cheNameToNum_cnt; ++i) {
+    a[x][y][i] += cnt[1][i];
+  }
+}
+
 void read() {
   std::string Input;
   std::getline(std::cin, Input);
-  int w = 0; int cnt = 1; std::string cur; int num = 0;
+  int w = 0;
+  int cnt = 1;
+  std::string cur;
   for (int i = 0; i < Input.size();) {
-	if (Input[i] == '+') {
-	  ++cnt;
-	  ++i;
-	} else if (Input[i] == '-' && Input[i + 1] == '>') {
-	  i += 2;
-	  len[w] = cnt;
-	  cnt = 1;
-	  w = 1;
-	} else {
-	  if (!isalpha(Input[i])) {
-		++i;
-		continue;
-	  }
-	  cur += Input[i++];
-	  while (Input[i] <= 'z' && Input[i] >= 'a') cur += Input[i++];
-	  while (Input[i] <= '9' && Input[i] >= '0') num = (num << 3) + (num << 1) + Input[i++] - '0';
-	  if (!num) ++num;
-	  a[w][cnt][addStringToMap(cur)] += num;
-	  cur.clear();
-	  num = 0;
-	}
+    if (Input[i] == '+') {
+      process(w, cnt, cur);
+      cur.clear();
+      ++cnt;
+      ++i;
+    } else if ((Input[i] == '-' && Input[i  + 1] == '>') || (Input[i] == '→')) {
+      process(w, cnt, cur);
+      cur.clear();
+      if (Input[i] == '→') ++i;
+      else i += 2;
+      len[w] = cnt;
+      cnt = 1;
+      w = 1;
+    } else {
+      if (!isalpha(Input[i]) && !isdigit(Input[i]) && Input[i] != '(' && Input[i] != ')') {
+        ++i;
+        continue;
+      }
+      cur += Input[i++];
+    }
   }
+  process(w, cnt, cur);
+  cur.clear();
   len[w] = cnt;
-/*  cnt = 1;
-  for (int i = 0; i <= 1; ++i) {
+  cnt = 1;
+  /*for (int i = 0; i <= 1; ++i) {
     for (int j = 1; j <= len[i]; ++j) {
-	  for (int k = 1; k <= cheNameToNum_cnt; ++k) {
-		std::cout << "(" << numToCheName[k] << "," << a[i][j][k] << ") ";
-	  }
-	  puts("");
-	}
-	puts("");
+      for (int k = 1; k <= cheNameToNum_cnt; ++k) {
+        std::cout << "(" << numToCheName[k] << "," << a[i][j][k] << ") ";
+      }
+      puts("");
+    }
+    puts("");
   }*/
 }
 
 void convertToMatrix() {
   for (int i = 0; i < 2; ++i) {
-	for (int j = 1; j <= len[i]; ++j) {
-	  for (int k = 1; k <= cheNameToNum_cnt; ++k) {
+    for (int j = 1; j <= len[i]; ++j) {
+      for (int k = 1; k <= cheNameToNum_cnt; ++k) {
         if (i == 0) {
-		  mtx[k][j] = a[i][j][k];
-		} else {
-		  mtx[k][j + len[0]] = -a[i][j][k];
-		}
-	  }
-	}
+          mtx[k][j] = a[i][j][k];
+        } else {
+          mtx[k][j + len[0]] = -a[i][j][k];
+        }
+      }
+    }
   }
 }
 
 std::vector<int> solve(int n, int m) {
   int curRow = 1;
   for (int j = 1; j < m; ++j) {
-	int t = -1;
-	for (int i = curRow; i <= n; ++i) {
-	  if (mtx[i][j] != 0) {
-		t = i;
-		break;
-	  }
-	}
-	if (t == -1) continue;
+    int t = -1;
+    for (int i = curRow; i <= n; ++i) {
+      if (mtx[i][j] != 0) {
+        t = i;
+        break;
+      }
+    }
+    if (t == -1) continue;
     for (int i = 1; i <= m; ++i) std::swap(mtx[t][i], mtx[curRow][i]);
-	for (int i = curRow + 1; i <= n; ++i) {
-	  if (mtx[i][j] == 0) continue;
-	  int tmp = mtx[curRow][j] * mtx[i][j] / gcd(mtx[curRow][j], mtx[i][j]);
-	  int t1 = tmp / mtx[curRow][j], t2 = tmp / mtx[i][j];
-	  for (int k = j; k <= m; ++k) {
-		mtx[i][k] = mtx[i][k] * t2 - mtx[curRow][k] * t1;
-	  }
-	}
-	++curRow;
+    for (int i = 1; i <= n; ++i) {
+      if (mtx[i][j] == 0 || i == curRow) continue;
+      int tmp = mtx[curRow][j] * mtx[i][j] / gcd(mtx[curRow][j], mtx[i][j]);
+      int t1 = tmp / mtx[curRow][j], t2 = tmp / mtx[i][j];
+      for (int k = 1; k <= m; ++k) {
+        mtx[i][k] = mtx[i][k] * t2 - mtx[curRow][k] * t1;
+      }
+    }
+    ++curRow;
   }
   if (curRow < m) return std::vector<int>{-1};
   std::vector<int> ans(m + 1);
 
-  int t = m;
+  int t = m - 1;
   while (mtx[n][m - 1] == 0) --n;
-  int tmp = gcd(std::abs(mtx[n][m - 1]), std::abs(mtx[n][m]));
-  ans[t--] = std::abs(mtx[n][m - 1]) / tmp;
-  ans[t--] = std::abs(mtx[n][m]) / tmp;
-  for (int i = n - 1; i >= 1 && t >= 1; --i, --t) {
-	int res = 0;
-	for (int j = t + 1; j <= m; ++j) {
-	  res += mtx[i][j] * ans[j];
-	}
-	if (res != 0) {
-	  ans[t] = (-res) / mtx[i][t];
-	} else {
-	  ans[t] = 1;
-	}
+
+  ans[m] = 1;
+  for (int i = n; i >= 1 && t >= 1; --i, --t) {
+    if (mtx[i][t] < 0) mtx[i][t] = -mtx[i][t], mtx[i][m] = -mtx[i][m];
+    int tmp = std::abs(gcd(mtx[i][t], mtx[i][m]));
+    mtx[i][t] /= tmp; mtx[i][m] /= tmp;
+    ans[m] = ans[m] * mtx[i][t] / gcd(ans[m], mtx[i][t]);
   }
+
+  t = m - 1;
+  for (int i = n; i >= 1 && t >= 1; --i, --t) {
+    ans[t] = ans[m] * (-mtx[i][m]) / mtx[i][t];
+  }
+
   return ans;
 }
